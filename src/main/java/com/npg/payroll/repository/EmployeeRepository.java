fbca18ece59e9a09d1b21f2b7c28c2bdb183f1d1
Boonyarit1998 <Boonyarit1998@gmail.com>
2026-01-17 13:31:25 +0700

    delete  service payroll and employee

--- src/main/java/com/npg/payroll/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.npg.payroll.repository;
-
-import com.npg.payroll.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
